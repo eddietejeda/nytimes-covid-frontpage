acd3f7dcda6f392b7abbd31b78f0966d116017f0
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-07-16 13:34:19 -0400

    simplified codebase

--- build.rb
+++ build.rb
@@ -4,9 +4,11 @@ require 'pdf-reader'
 require 'open-uri'
 require 'json'
 
-def download_nytimes_frontpage(start_date, end_date, path)
+
+
+def download_nytimes_frontpage(start_date, end_date)
   ( start_date .. end_date ).each do |current|
-    filename = "#{path}/#{current.to_s}.pdf"
+    filename = File.join("data", "pdfs", "#{current.to_s}.pdf")
     unless File.file? filename
       puts "downloading... #{current.to_s}"
       File.open(filename, "wb") do |file|
@@ -16,21 +18,23 @@ def download_nytimes_frontpage(start_date, end_date, path)
   end
 end
 
-def count_words_in_pdf(path)
+def generate_word_count_from_pdf
+  puts "generating word count..."
   results = {}
-  Dir["#{path}/*.pdf"].each do |filename|
+  Dir[File.join("data", "pdfs", "*.pdf")].each do |filename|
     reader = PDF::Reader.new(filename)
     reader.pages.each do |page|
       current_time = Date.parse(File.basename(filename, File.extname(filename))).to_time.to_i      
       results.merge!("#{current_time}": page.text.downcase.scan(/(?=(corona|covid|virus|pandemic|wuhan))/).count)
     end
   end
+  puts "processed #{results.count} items"
   results
 end
 
-def save_json(results, filename)
-  File.open(filename,"w") do |f|
-    f.write(JSON.generate(results))
+def save_json(data, filename)
+  File.open(File.join("data",filename),"w") do |f|
+    f.write(JSON.generate(data))
   end
 end
 
@@ -38,7 +42,5 @@ end
 start_date = DateTime.parse('2019-12-01').to_date # A month before the first covid case
 end_date = Date.today
 
-download_nytimes_frontpage(start_date, end_date, "./download")
-save_json(count_words_in_pdf("./download"), "results.json")
-
-
+download_nytimes_frontpage(start_date, end_date)
+save_json(generate_word_count_from_pdf, "results.json")
